32bdc641b5d135f54ce6735534f101791d776687
funaaya <150566081+funaaya@users.noreply.github.com>
2024-01-28 09:14:09 +0900

    提出１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
     @name = current_user.name
     @profile=current_user.profile
     @occupation=current_user.occupation
